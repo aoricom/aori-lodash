cb1d776ddda899d1d90853b541bbc6bf3df0e425
madietov <MAdietov@blondinka.ru>
2018-08-10 15:54:04 +0300

    whole bundle

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import node from "rollup-plugin-node-resolve";
+import minify from 'rollup-plugin-minify-es';
+
+
+
+export default {
+  input: "node_modules/lodash-es/lodash.default.js",
+  output: {
+    name: "lodash",
+    format: "esm",
+    file: "dist/lodash.js"
+  },
+  plugins: [
+    node({ jsnext: true }),
+    minify()
+    ]
+};
